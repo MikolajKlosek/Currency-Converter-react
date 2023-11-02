07050d6cd9b4fe2239b0f90d5a56773f96086c4f
Mikolaj Klosek <mikolaj.klosek@gmail.com>
2023-11-02 17:33:07 +0100

    interactions added

---
+++ src/App/Form/index.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { currencies } from "../currencies";
+import { Result } from "./Result";
+import "./style.css";
+
+export const Form = ({ calculateResult, result }) => {
+    const [currency, setCurrency] = useState(currencies[0].short);
+    const [amount, setAmount] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(currency, amount);
+    }
+
+    return (
+        <form className="form" onSubmit={onSubmit}>
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">
+                    Przelicz walutę
+                </legend>
+                <p>
+                    <label>
+                        <span className="form_labelText">Kwota w PLN*:
+                        </span>
+                        <input
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            className="form__field"
+                            type="number"
+                            step="0.01"
+                            placeholder="Wpisz kwotę w zł"
+                            required
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form_labelText">
+                            Waluta:
+                        </span>
+                        <select
+                            className="form__field"
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                        >
+                            {currencies.map((currency => (
+                                <option
+                                    key={currency.short}
+                                    value={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            )))}
+                        </select>
+                    </label>
+                </p>
+            </fieldset>
+            <p>
+                <button className="form__button">Przelicz!</button>
+            </p>
+
+            <Result result={result} />
+        </form>
+    );
+};
---
+++ src/App/currencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.43,
+    },
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4.02,
+    },
+    {
+        short: "AED",
+        name: "Dirham Zjednoczonych Emiratów Arabskich",
+        rate: 1.09,
+    },
+    {
+        short: "NOK",
+        name: "Korona Norweska",
+        rate: 0.40,
+    },
+];
