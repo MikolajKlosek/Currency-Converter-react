727dafe726d430a0a8c7c5e8880088e41a1cdfa1
Mikolaj Klosek <mikolaj.klosek@gmail.com>
2023-11-20 21:46:32 +0100

    added themeprovider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from './GlobalStyle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle>
-      <App />
-    </GlobalStyle>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle>
+        <App />
+      </GlobalStyle>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
