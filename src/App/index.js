2e4693a9fbf24a8cd1cd6eec7ac5bd1dcb7dc246
Mikolaj Klosek <mikolaj.klosek@gmail.com>
2023-12-02 13:18:01 +0100

    edited index.js in App

--- src/App/index.js
+++ src/App/index.js
@@ -1,31 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Form } from './Form';
-import { currencies } from './currencies'
 import { Clock } from './Clock'
 import { Wrapper } from './styled';
 
 function App() {
-    const [result, setResult] = useState();
-
-    const calculateResult = (currency, amount) => {
-        const rate = currencies
-            .find(({ short }) => short === currency)
-            .rate;
-
-        setResult({
-            sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency,
-        });
-    }
 
     return (
         <Wrapper>
             <Clock />
-            <Form
-                result={result}
-                calculateResult={calculateResult}
-            />
+            <Form />
         </Wrapper>
     );
 }
